0d8e8f3dd8c7d190c78d309a9d3237799ba1bf72
Amaury Bréhéret <abreheret@gmail.com>
2017-10-05 11:22:15 +0200

    update content of about_dialog.

--- src/about_dialog.cpp
+++ src/about_dialog.cpp
@@ -39,21 +39,27 @@ AboutDialog::AboutDialog(QWidget *parent):QDialog(parent) {
 	
 	QTextBrowser *content = new QTextBrowser(this);
 	QString text = QString::fromLatin1(
-		"<html><head><meta name = \"qrichtext\" content = \"1\"/>"
-		"<style type = \"text/css\">p, li{ white - space: pre - wrap; }"
-		"</style></head><body style = \" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">"
-		"<p style = \" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style = \" font-size:10pt;\">Version 1.0.0 </span></p>"
-		"<p style = \" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style = \" font-size:10pt;\">Author : </span><span style = \" font-size:10pt; font-weight:600;\">Amaury Bréhéret </span><span style = \" font-size:10pt;\">(abreheret@gmail.com)</span></p>"
-		"<a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8K79VKWBS7352\"><span style=\" font-size:12pt; text-decoration: underline; color:#0000ff;\">Donate</span></a><span style=\" font-size:10pt;\"></span></p>"
-		"<p style = \"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br/></p>"
-		"<p style = \" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style = \" font-size:10pt;\">Shortcuts : </span></p>"
-		"<p style = \" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style = \" font-size:10pt;\">-CRTL + Mouse Wheel : Zoom image</span></p>"
-		"<p style = \" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style = \" font-size:10pt;\">-SHIFT + Mouse Wheel : Changing the circle size of brush</span></p>"
-		"<p style = \" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style = \" font-size:10pt;\">-SPACE : Run watershed button</span></p>"
-		"<p style = \" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style = \" font-size:10pt;\">-RIGHT MOUSE CLICK on image : Captures and selects the label at the mouse coordinate</span></p>"
-		"</body></html>" );
+	"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">"
+	"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">"
+	"p, li { white-space: pre-wrap; }"
+	"</style></head>"
+		"<body style=\" font-family:'MS Shell Dlg 2'; font-size:8pt; font-weight:400; font-style:normal;\">"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Version 1.0.0</span></p>"
+		"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Author: </span><span style=\" font-size:10pt; font-weight:600;\">Amaury Bréhéret</span></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Software use :  </span><a href=\"http://opencv.org/\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">OpenCV</span></a><span style=\" font-size:10pt;\"> and </span><a href=\"https://www.qt.io/\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Qt</span></a><span style=\" font-size:10pt;\">.</span></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Make donation : </span><a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=8K79VKWBS7352\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Donate</span></a></p>"
+		"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Shortcuts : </span></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">- CTRL + Mouse Wheel: zoom image</span></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">- SHIFT + Mouse Wheel: Changing cicle size of brush</span></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">- SPACE: Run watershed algorithm</span></p>"
+		"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">- Right Mouse Click on image : Captures and selects the label at the mouse coordinate.</span></p>"
+		"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>"
+	"</body></html>");
 
 	content->setText(text);
+	resize(QSize(600,300));
 
 	a->setWidgetResizable(true);
 	a->setWidget(content);
